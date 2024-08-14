fa566448c9c2151243db24d98598eba33591da4d
Abhi Vijaykumar <abhivijaykumar@Abhis-MacBook-Pro.local>
2024-08-14 14:49:08 -0400

    Initial project setup with Flask and React

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Home(){
+  return <h1>Welcome to the Itinerary Planner!</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Home />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
